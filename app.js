7f15d953a39d38b88217edcfebccd8386be94fb3
Edmond Luu <eluu472@gmail.com>
2022-06-11 17:16:11 -0700

    Update

--- app.js
+++ app.js
@@ -16,10 +16,10 @@ selectorBox.addEventListener('change', () => {
     boxValue = selectorBox.value
     switch (boxValue) {
         case 'cel':
-            titleText.innerText = "Celcius to Farenheit";
+            titleText.innerText = "Celsius to Farenheit";
             break;
         case 'far':
-            titleText.innerText = "Farenheit to Celcius"
+            titleText.innerText = "Farenheit to Celsius"
             break;
     }
 
@@ -27,13 +27,13 @@ selectorBox.addEventListener('change', () => {
 
 
 function cToF() {
-    let celcius = input.value;
-    if (!parseInt(celcius)) {
-        celcius = 0;
+    let celsius = input.value;
+    if (!parseInt(celsius)) {
+        celsius = 0;
     }
-    const farenheit = ((celcius * 9) / 5) + 32;
-    const kelvin = (parseFloat(celcius) + 273.15).toFixed(2);
-    const conversionResult = `${celcius} °C ≈ ${farenheit} °F ≈ ${kelvin} K`;
+    const farenheit = ((celsius * 9) / 5) + 32;
+    const kelvin = (parseFloat(celsius) + 273.15).toFixed(2);
+    const conversionResult = `${celsius} °C ≈ ${farenheit} °F ≈ ${kelvin} K`;
     const data = document.createElement("h3");
     data.innerText = conversionResult;
     results.append(data);
@@ -45,9 +45,9 @@ function fToC() {
     if (!parseInt(farenheit)) {
         farenheit = 0;
     }
-    const celcius = (((5 * (farenheit - 32)) / 9)).toFixed(2);
-    const kelvin = (parseFloat(celcius) + 273.15).toFixed(2);
-    const conversionResult = `${farenheit} °F ≈ ${celcius} °C ≈ ${kelvin} K`;
+    const celsius = (((5 * (farenheit - 32)) / 9)).toFixed(2);
+    const kelvin = (parseFloat(celsius) + 273.15).toFixed(2);
+    const conversionResult = `${farenheit} °F ≈ ${celsius} °C ≈ ${kelvin} K`;
     const data = document.createElement("h3");
     data.innerText = conversionResult;
     results.append(data);
